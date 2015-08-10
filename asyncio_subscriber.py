35e60ab1c2c60518fec533f1eb865372c58ebffb
KIU Shueng Chuan <nixchuan@gmail.com>
2015-08-10 18:56:02 +0800

    use read_exactly(), no need accum_buffer

--- asyncio_subscriber.py
+++ asyncio_subscriber.py
@@ -10,27 +10,18 @@ def subscribe_stuff():
     reader, writer = yield from asyncio.open_connection('127.0.0.1', 10000)
 
     hdrfmt = struct.Struct('>I')
-    wait_hdr = True
-    packet_size = hdrfmt.size
-    accum_buffer = bytearray()
 
     while True:
-        payload = yield from reader.read(8192)
-        if not payload:
+        try:
+            packet_size = hdrfmt.size
+            hdr = yield from reader.readexactly(packet_size)
+            packet_size = hdrfmt.unpack(hdr)[0]
+            payload = yield from reader.readexactly(packet_size)
+        except asyncio.IncompleteReadError as e:
+            print('partial read {} / {}'.format(len(e.partial), packet_size))
             break
 
-        accum_buffer.extend(payload)
-
-        while len(accum_buffer) >= packet_size:
-            pkt = memoryview(accum_buffer)[:packet_size]
-            accum_buffer = accum_buffer[packet_size:]
-            if wait_hdr:
-                wait_hdr = False
-                packet_size = hdrfmt.unpack(pkt)[0]
-            else:
-                wait_hdr = True
-                packet_size = hdrfmt.size
-                handle_msg(pkt)
+        handle_msg(payload)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(subscribe_stuff())
